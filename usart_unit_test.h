d6f98e770f75511865c952ed2392c3ad3be13317
Jakub Brzezowski <44787968+zdzwie@users.noreply.github.com>
2019-03-22 14:53:14 +0100

    Create usart_unit_test.h
    USART Test

---
+++ usart_unit_test.h
@@ -0,0 +1,10 @@
+#ifndef _USART_UNIT_TEST_H_
+#define _USART_UNIT_TEST_H_
+
+/**
+ * Runs a unit test of the USART implementation. The test results are obtainted
+ * through an external PC test program.
+ */
+void USART_UnitTest(void);
+
+#endif
